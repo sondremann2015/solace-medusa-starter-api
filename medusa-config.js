4b034a8b0b396d1f8158c87370d74b1fc88c2980
Veskel01 <jakubandrz1@gmail.com>
2024-10-10 15:04:12 +0200

    feat(AC-100): configure dynamic modules

--- medusa-config.js
+++ medusa-config.js
@@ -2,10 +2,31 @@ const { loadEnv, defineConfig, Modules } = require('@medusajs/framework/utils');
 
 loadEnv(process.env.NODE_ENV, process.cwd());
 
+const dynamicModules = {};
+
 const stripeApiKey = process.env.STRIPE_API_KEY;
 const stripeWebhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
 const isStripeConfigured = Boolean(stripeApiKey) && Boolean(stripeWebhookSecret);
+
+if (isStripeConfigured) {
+  console.log('Stripe aki key and webhook secret found. Enabling payment module');
+  dynamicModules[Modules.PAYMENT] = {
+    resolve: '@medusajs/medusa/payment',
+    options: {
+      providers: [
+        {
+          resolve: '@medusajs/medusa/payment-stripe',
+          id: 'stripe',
+          options: {
+            apiKey: stripeApiKey,
+            webhookSecret: stripeWebhookSecret
+          }
+        }
+      ]
+    }
+  };
+}
 
 module.exports = defineConfig({
   projectConfig: {
@@ -23,27 +44,7 @@ module.exports = defineConfig({
     }
   },
   modules: {
-    ...(isStripeConfigured
-      ? {
-          [Modules.PAYMENT]: {
-            resolve: '@medusajs/medusa/payment',
-            options: {
-              providers: [
-                {
-                  resolve: '@medusajs/medusa/payment-stripe',
-                  id: 'stripe',
-                  options: {
-                    apiKey: stripeApiKey,
-                    webhookSecret: stripeWebhookSecret
-                  }
-                }
-              ]
-            }
-          }
-        }
-      : {})
-  },
-  modules: {
+    ...dynamicModules,
     [Modules.FILE]: {
       resolve: '@medusajs/medusa/file',
       options: {
