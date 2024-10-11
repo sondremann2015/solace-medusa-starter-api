873b1f776df606980da2b4623705d4cab0da6cc7
Veskel01 <jakubandrz1@gmail.com>
2024-10-11 12:17:19 +0200

    fix(AC-100): fix deployment

--- medusa-config.js
+++ medusa-config.js
@@ -10,7 +10,7 @@ const stripeWebhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 const isStripeConfigured = Boolean(stripeApiKey) && Boolean(stripeWebhookSecret);
 
 if (isStripeConfigured) {
-  console.log('Stripe aki key and webhook secret found. Enabling payment module');
+  console.log('Stripe API key and webhook secret found. Enabling payment module');
   dynamicModules[Modules.PAYMENT] = {
     resolve: '@medusajs/medusa/payment',
     options: {
@@ -28,6 +28,47 @@ if (isStripeConfigured) {
   };
 }
 
+const modules = {
+  [Modules.FILE]: {
+    resolve: '@medusajs/medusa/file',
+    options: {
+      providers: [
+        {
+          resolve: '@medusajs/file-s3',
+          id: 's3',
+          options: {
+            file_url: process.env.DO_SPACE_URL,
+            access_key_id: process.env.DO_SPACE_ACCESS_KEY,
+            secret_access_key: process.env.DO_SPACE_SECRET_KEY,
+            region: process.env.DO_SPACE_REGION,
+            bucket: process.env.DO_SPACE_BUCKET,
+            endpoint: process.env.DO_SPACE_ENDPOINT
+          }
+        }
+      ]
+    }
+  },
+  [Modules.NOTIFICATION]: {
+    resolve: '@medusajs/medusa/notification',
+    options: {
+      providers: [
+        {
+          resolve: './modules/resend-notification',
+          id: 'resend-notification',
+          options: {
+            channels: ['email'],
+            apiKey: process.env.RESEND_API_KEY,
+            fromEmail: process.env.RESEND_FROM_EMAIL,
+            replyToEmail: process.env.RESEND_REPLY_TO_EMAIL,
+            toEmail: process.env.TO_EMAIL,
+            enableEmails: process.env.ENABLE_EMAIL_NOTIFICATIONS
+          }
+        }
+      ]
+    }
+  }
+};
+
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
@@ -41,28 +82,13 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS,
       jwtSecret: process.env.JWT_SECRET || 'supersecret',
       cookieSecret: process.env.COOKIE_SECRET || 'supersecret'
+    },
+    admin: {
+      disable: process.env.DISABLE_MEDUSA_ADMIN === 'true'
     }
   },
   modules: {
     ...dynamicModules,
-    [Modules.FILE]: {
-      resolve: '@medusajs/medusa/file',
-      options: {
-        providers: [
-          {
-            resolve: '@medusajs/medusa/file-s3',
-            id: 's3',
-            options: {
-              file_url: process.env.DO_SPACE_URL,
-              access_key_id: process.env.DO_SPACE_ACCESS_KEY,
-              secret_access_key: process.env.DO_SPACE_SECRET_KEY,
-              region: process.env.DO_SPACE_REGION,
-              bucket: process.env.DO_SPACE_BUCKET,
-              endpoint: process.env.DO_SPACE_ENDPOINT
-            }
-          }
-        ]
-      }
-    }
+    ...modules
   }
 });
